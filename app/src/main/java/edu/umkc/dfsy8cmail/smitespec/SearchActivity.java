cb00b97d7910fa16eaf10e1db2f2ba45012ffddd
Dan Stucky <danfstucky@gmail.com>
2015-09-19 12:59:45 -0500

    API added and working

--- app/src/main/java/edu/umkc/dfsy8cmail/smitespec/SearchActivity.java
+++ app/src/main/java/edu/umkc/dfsy8cmail/smitespec/SearchActivity.java
@@ -1,16 +1,37 @@
 package edu.umkc.dfsy8cmail.smitespec;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.cr5315.jSmite.Smite;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchActivity";
+
+    Smite smite = new Smite("1517", "4FA5E41C82DC4F718A00A3B074F22658");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        new FetchPlayerTask().execute();
+        //String status = smite.getPlayerStatus("Kaitrono");
+       // Log.d(TAG, status);
+    }
+
+    private class FetchPlayerTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String status = smite.getPlayerStatus("Kaitrono");
+            Log.i(TAG, "Fetched player status: " + status);
+            return null;
+        }
     }
 
     @Override
