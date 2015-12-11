17569c75df47fa12dfde768264d37d8cd3fb680d
Dan Stucky <danfstucky@gmail.com>
2015-12-11 10:58:45 -0600

    Iteration 6: Add action bar items and refactor some code.

---
+++ app/src/main/java/edu/umkc/dfsy8cmail/smitespec/HomeBar.java
@@ -0,0 +1,7 @@
+package edu.umkc.dfsy8cmail.smitespec;
+
+/**
+ * Created by Dan on 12/10/2015.
+ */
+public class HomeBar {
+}
---
+++ app/src/main/java/edu/umkc/dfsy8cmail/smitespec/MenuBar.java
@@ -0,0 +1,7 @@
+package edu.umkc.dfsy8cmail.smitespec;
+
+/**
+ * Created by Dan on 12/9/2015.
+ */
+public class MenuBar {
+}
