d2e6f2eea2bbdf405dfddd1827b9f34279171885
Dan Stucky <danfstucky@gmail.com>
2017-03-31 18:38:18 -0500

    Add missing gods and update android

---
+++ app/src/main/java/edu/umkc/dfsy8cmail/smitespec/API.java
@@ -0,0 +1,14 @@
+package edu.umkc.dfsy8cmail.smitespec;
+
+/**
+ * Created by Dan on 12/9/2015.
+ */
+public class API {
+    private String mDevID = "1517";
+    private String mAuthKey = "4FA5E41C82DC4F718A00A3B074F22658";
+
+    public String getDevID() { return mDevID; }
+
+    public String getAuthKey() { return mAuthKey; }
+
+}
